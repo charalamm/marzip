95ef15721c1ed600536059a84c430467bb4f0341
Charalampos Malathounis <charalamm@protonmail.com>
2021-01-26 01:57:50 +0200

    first

---
+++ data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-8hw0clw-360-panorama-miami",
+      "name": "8hw0clw-360-panorama-miami",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.825075345123258,
+        "pitch": 0.07046351624681613,
+        "fov": 1.2419901288725592
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3439534255922254,
+          "pitch": -0.13745594221976987,
+          "rotation": 3.141592653589793,
+          "target": "1-tu09tpc-a-large-bed-in-a-room"
+        },
+        {
+          "yaw": 0.8624548100971374,
+          "pitch": 0.26093790410116213,
+          "rotation": 0,
+          "target": "1-tu09tpc-a-large-bed-in-a-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-tu09tpc-a-large-bed-in-a-room",
+      "name": "tu09tpc-a-large-bed-in-a-room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1718.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-8hw0clw-360-panorama-miami"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
